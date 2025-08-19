7ca59ab6a5f23ca5199061a2e76e08c4a7a1291f
zzenku <terebovz@mail.ru>
2025-08-19 18:52:08 +0300

    aggregate anf annotate

--- service/services/serializers.py
+++ service/services/serializers.py
@@ -18,8 +18,7 @@ class SubscriptionSerializer(ModelSerializer):
     price = SerializerMethodField()
 
     def get_price(self, instance):
-        return (instance.service.full_price -
-                instance.service.full_price * (instance.plan.discount_percent / 100))
+        return instance.price
 
     class Meta:
         model = Subscription
