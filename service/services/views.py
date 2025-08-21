0d094589268031edaf860fecae2bec64ccce111f
zzenku <terebovz@mail.ru>
2025-08-21 15:32:55 +0300

    optimization of price with celery task

--- service/services/views.py
+++ service/services/views.py
@@ -10,9 +10,7 @@ class SubscriptionView(ReadOnlyModelViewSet):
     queryset = Subscription.objects.all().prefetch_related(
         'plan',
         Prefetch('client',
-                 queryset=Client.objects.all().select_related('user').only('company_name', 'user__email'))
-    ).annotate(price=F('service__full_price') -
-                     F('service__full_price') * F('plan__discount_percent') / 100.00)
+                 queryset=Client.objects.all().select_related('user').only('company_name', 'user__email')))
     serializer_class = SubscriptionSerializer
 
     def list(self, request, *args, **kwargs):
